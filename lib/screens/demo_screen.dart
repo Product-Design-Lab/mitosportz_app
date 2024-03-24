259bfaa736f634172cfc18ddca4235f3700ac629
Carlos Melegrito <cj@mlgrto.com>
2024-03-24 18:38:22 +1100

    Create Slider Input Widget

--- lib/screens/demo_screen.dart
+++ lib/screens/demo_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:mitosportz/widgets/base.dart';
+import 'package:mitosportz/widgets/slider_input_widget.dart';
 
 class DemoScreen extends StatelessWidget {
   const DemoScreen({super.key});
 
+  void action(double value) {
+    print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Base(child: Text("Hello World"));
+    return Base(
+        child: SliderInputWidget(
+            title: "Title", label: "%", action: action, actionText: "Submit"));
   }
 }
---
+++ lib/widgets/slider_input_widget.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+import 'package:mitosportz/constants/button_styles.dart';
+import 'package:mitosportz/constants/colors.dart';
+
+import 'package:mitosportz/constants/text_styles.dart';
+import 'package:mitosportz/constants/widget_styles.dart';
+
+class SliderInputWidget extends StatefulWidget {
+  final String title;
+  final String label;
+  final ValueSetter<double> action;
+  final String actionText;
+  final Function()? altAction;
+  final String? altActionText;
+
+  const SliderInputWidget({
+    Key? key,
+    required this.title,
+    required this.label,
+    required this.action,
+    required this.actionText,
+    this.altAction,
+    this.altActionText,
+  }) : super(key: key);
+
+  @override
+  State<SliderInputWidget> createState() => _SliderInputWidgetState();
+}
+
+class _SliderInputWidgetState extends State<SliderInputWidget> {
+  double _value = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget slider() {
+    return Slider(
+      min: 0,
+      max: 100,
+      value: _value,
+      inactiveColor: AppColors.background,
+      onChanged: (value) {
+        setState(() {
+          _value = value;
+        });
+      },
+    );
+  }
+
+  Widget inputCard() {
+    return Expanded(
+        child: Container(
+      decoration: WidgetStyles.elevatedCard,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(widget.title, style: TextStyles.title),
+            Padding(
+                padding: const EdgeInsets.only(
+                    top: 48, bottom: 48, right: 16, left: 16),
+                child: slider()),
+            Text("${_value.round()}${widget.label}",
+                style: TextStyles.body, textAlign: TextAlign.center),
+          ]),
+    ));
+  }
+
+  Widget submitButton() {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {
+          widget.action(_value);
+        },
+        style: ButtonStyles.buttonPrimary,
+        child: Text(widget.actionText),
+      ),
+    );
+  }
+
+  Widget altButton() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16),
+        child: ElevatedButton(
+            onPressed: widget.altAction,
+            style: ButtonStyles.buttonSecondary,
+            child: Text("${widget.altActionText}")),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          inputCard(),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Row(children: [
+              submitButton(),
+              if (widget.altAction != null) altButton()
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
